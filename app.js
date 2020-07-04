fe65b9f141991ba0ee9f70cd324d316b2f047290
cruftous <64802578+cruftous@users.noreply.github.com>
2020-07-03 19:37:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ hdr.addEventListener("click",(e) => {
   console.log('something works here!');
   e.target.innerHTML = "Welcome!";
 });
+
+if (window.location.href === "https://cruftous.github.io/analytics") {
+  alert("You are on the original site!");
+} else {
+  alert("You are not on the original site!");
+}
